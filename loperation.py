32d6f0175d60e1f2612607d803a717e029485b44
Vaishnavi Patil <vaishkpatil1217@gmail.com>
2025-12-09 15:59:21 +0530

    accese,lenth

---
+++ loperation.py
@@ -0,0 +1,11 @@
+no = [10, 20, 30, 40, 50]
+print("third element is",no[2]) #accese  elment 
+print("no of element is",len(no)) #list lenth 
+
+if len == 0:                        
+    print("list is empty ")         #check if emty
+else:
+    print("list is not empty")
+    
+    
+   
